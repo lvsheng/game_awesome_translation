8c47c3cbf689a55f12005c15bdf1875257acbdf0
lvsheng <lvsheng@baidu.com>
2014-12-22 21:58:17 +0800

    引导结束后引导层将自己从父节点上移除

--- src/bunt/gameScene/GuideLayer.js
+++ src/bunt/gameScene/GuideLayer.js
@@ -5,18 +5,32 @@ define([
     '../../gameUtil/resourceFileMap'
 ], function (resourceFileMap) {
     return cc.Layer.extend({
+        _onStartGame: null,
+
         /**
          * @constructor
-         * @param startGame 回调函数，用户确认跳过引导层时执行
+         * @param onStartGame 回调函数，用户确认跳过引导层时执行
          */
-        ctor: function (startGame) {
-            this._super();
-            this.init();
+        ctor: function (onStartGame) {
+            var self = this;
+            self._super();
+            self.init();
+
+            self._onStartGame = onStartGame;
 
             setTimeout(function (){
                 alert("快速点击屏幕右侧以驱动你的挖掘机");
-                startGame();
-            })
+                self._startGame();
+            });
+        },
+
+        /**
+         * 结束引导，开始游戏
+         * @private
+         */
+        _startGame:function () {
+            this.parent.removeChild(this);
+            this._onStartGame();
         }
     });
 });
