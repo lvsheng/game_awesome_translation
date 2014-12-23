1350d83e6af0ecf1c733e6dfe4c8c79730bffaa1
lvsheng <lvsheng@baidu.com>
2014-12-23 23:05:55 +0800

    加一些TODO

--- src/bunt/gameScene/gameLayer/ComputerControl.js
+++ src/bunt/gameScene/gameLayer/ComputerControl.js
@@ -7,6 +7,7 @@ define([], function () {
     return cc.Node.extend({
         _positionManager: null,
         //key为剩余比例。1为还剩10%以内，5为中央
+        //TODO: 改策略：从远到近会加快其速度，但从近到远不再再降低其速度
         _HIT_INTERVAL_MAP: {10: B, 9: B, 8: B, 7: B, 6: B, 5: B - 0.01, 4: B - 0.01, 3: B - 0.02, 2: B - 0.03, 1: B - 0.04},
         _hitInterval: 0,
         ctor: function (positionManager) {
--- src/main.js
+++ src/main.js
@@ -17,4 +17,6 @@ require([
     cc.game.run("gameCanvas");
 
     //TODO: 用户旋转屏幕的处理、对水平屏幕的要求。旋转处理时应先暂停游戏、不能影响游戏（比如bunt游戏中就是根据屏幕宽度来判断胜负的）
+    //TODO: bug to fix: 在android下刚进入页面时点得快了可能使屏幕放大
+    //TODO: 有些浏览器下无法横屏？
 });
