252f0468100690ff1fef4387b77e61d82aa2e4c0
lvsheng <lvsheng@baidu.com>
2014-12-23 09:42:35 +0800

    使背景层按窗口进行缩放以将窗口完全cover（在适配方式为cc.ResolutionPolicy.FIXED_HEIGHT的情况下）

--- src/bunt/gameScene/BackgroundLayer.js
+++ src/bunt/gameScene/BackgroundLayer.js
@@ -8,7 +8,17 @@ define([
 
             var backgroundSprite = new cc.Sprite(resourceFileMap.bunt.bg);
             backgroundSprite.setPosition(cc.director.getWinSize().width / 2, cc.director.getWinSize().height / 2);
+            this._scaleToCoverWindow(backgroundSprite);
             this.addChild(backgroundSprite);
+        },
+
+        _scaleToCoverWindow: function (sprite) {
+            //先试着以等高进行缩放
+            var scale = cc.director.getWinSize().height / sprite.height;
+            //水平方向有黑边、换以等宽进行缩放
+            if (sprite.width * scale < cc.director.getWinSize().width) { scale = cc.director.getWinSize().width / sprite.width; }
+
+            sprite.scale = scale;
         }
     });
 });
