5dbf4c8441ecb9a82b04009ec614902fb1d26ae3
lvsheng <lvsheng@baidu.com>
2014-12-24 10:55:03 +0800

    调整走到屏幕一端时的翻转效果

--- src/avoid/gameScene/gameLayer/Teenager.js
+++ src/avoid/gameScene/gameLayer/Teenager.js
@@ -19,19 +19,31 @@ define([
             this._move_range_left = this._move_range_margin;
             this._move_range_right = cc.director.getWinSize().width - this._move_range_margin;
             this._oneTripTime = 4.5; //从一端走到另一端需要的时间
+            //TODO: for test
+            this._oneTripTime = this._oneTripTime / 4; //从一端走到另一端需要的时间
 
+            //TODO: 后面有必要时，看把anchorX放在女孩身体中央，这样翻转时看起来可能效果好些。然后_crashRangeWidth分成left与right
             this.attr({ anchorX: 0.42, x: this._move_range_right, anchorY: 0, y: 0 }); //anchor放在男孩女孩中间
 
             this._startMove();
+
+            //TODO: for debug
+            window.teenager = this;
         },
 
         _startMove: function () {
             var self = this;
             self.runAction(new cc.RepeatForever(new cc.Sequence(
                 new cc.MoveTo(self._oneTripTime, self._move_range_left, self.y), //认为一定是从_move_range_right开始移动的
-                new cc.CallFunc(function(){ self.flippedX = true; }),
+                new cc.CallFunc(function(){
+                    self.flippedX = true;
+                    self.anchorX = 1 - self.anchorX; //flip只是让绘制的内容翻转、不影响位置与anchor。这里把anchor跟随做翻转
+                }),
                 new cc.MoveTo(self._oneTripTime, self._move_range_right, self.y),
-                new cc.CallFunc(function(){ self.flippedX = false; })
+                new cc.CallFunc(function(){
+                    self.anchorX = 1 - self.anchorX;
+                    self.flippedX = false;
+                })
             )));
         },
 
