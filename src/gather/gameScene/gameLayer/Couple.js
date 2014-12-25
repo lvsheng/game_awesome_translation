339c4d8e3ec2d9cc7d0ccc7eb7032e97f0d52304
lvsheng <lvsheng@baidu.com>
2014-12-26 01:32:47 +0800

    增加了移近、移开的接口

--- src/gather/gameScene/gameLayer/Couple.js
+++ src/gather/gameScene/gameLayer/Couple.js
@@ -6,42 +6,53 @@ define([
     '../../../gameUtil/resourceFileMap'
 ], function (resourceFileMap) {
     return cc.Node.extend({
-        _hitCount: 0,
         /**
-         * @param meetCallback
-         * @param outCallback
+         * @param endCallback {Function} 调用时传入参数: 'meet'|'out'
          * @param initDistance {number} 0~1的数字，1代表整个屏幕的长度
          * @param initSpeed {number} 0~1的数字，1代表整个屏幕的长度
          */
-        ctor: function(meetCallback, outCallback, initDistance, initSpeed){
-            this._super(); this.init();
+        ctor: function(endCallback, initDistance, initSpeed){
+            var self = this;
+            self._super(); self.init();
 
-            this._meetCallback = meetCallback;
-            this._outCallback = outCallback;
-            this._speed = initSpeed;
-            this._distance = initDistance; //双方之间的距离，0~1
-            this._left = new cc.Sprite(resourceFileMap.gather.left);
-            this._right = new cc.Sprite(resourceFileMap.gather.right);
+            self._endCallback = endCallback;
+            self._speed = initSpeed;
+            self._distance = initDistance; //双方之间的距离。距离与left、right的位置绑定，每次更新distance，同步更新left、right位置
+            self._left = new cc.Sprite(resourceFileMap.gather.left);
+            self._right = new cc.Sprite(resourceFileMap.gather.right);
 
-            this._left.anchorY = this._right.anchorY = 0;
+            self._left.anchorY = self._right.anchorY = 0;
+            self.addChild(self._left);
+            self.addChild(self._right);
+            self._updatePosition();
 
-            this.addChild(this._left);
-            this.addChild(this._right);
-
-            this._startSeparate();
+            self.schedule(function (dt) { self.separate(self._speed * dt); });
         },
 
         setSpeed: function (speed) { this._speed = speed },
+        closeUp: function (distance) {
+            this._setDistance(this._distance - distance);
+            if (this._isMeet()) {
+                this._setDistance(this._getMeetDistance());
+                this._endCallback('meet');
+            }
+        },
+        separate: function (distance) {
+            this._setDistance(this._distance + distance);
+            if (this._distance > 1) {
+                this._setDistance(1);
+                this._endCallback('out');
+            }
+        },
 
-        //开始让两个小人向左右分离
-        _startSeparate: function () {
-            var self = this;
-            self.schedule(function (dt) {
-                self._distance += self._speed * dt;
-
-                if (self._distance < 1) { self._updatePosition(); }
-                else { self.unschedule(arguments.callee); self._outCallback(); }
-            });
+        _isMeet: function () { return this._distance <= this._getMeetDistance; },
+        _getMeetDistance: function () {
+            var px = (this._left.width / 2 + this._right.width / 2) * 0.8;
+            return px / cc.director.getWinSize().width;
+        },
+        _setDistance: function (distance) {
+            this._distance = distance;
+            this._updatePosition();
         },
         _updatePosition: function () {
             var winWidth = cc.director.getWinSize().width;
