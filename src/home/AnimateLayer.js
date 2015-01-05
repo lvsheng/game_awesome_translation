38d15e7032756a4e4605a0d70da116ac1e090ab1
lvsheng <lvsheng@baidu.com>
2015-01-05 09:52:17 +0800

    首页动画初步尝试

--- src/home/AnimateLayer.js
+++ src/home/AnimateLayer.js
@@ -17,14 +17,15 @@ define([
                 null
             ));
 
-            this.addChild(this._board = new cc.Sprite(resourceFileMap.home.board));
-            this.addChild(this._title = new cc.Sprite(resourceFileMap.home.title));
-            this.addChild(this._leftPerson = new cc.Sprite(resourceFileMap.home.leftPerson));
-            this.addChild(this._rightPerson = new cc.Sprite(resourceFileMap.home.rightPerson));
-            this.addChild(this._ribbon = new cc.Sprite(resourceFileMap.home.ribbon));
-            this.addChild(this._buttonMenu = buttonMenu);
-            this.addChild(this._horn = new cc.Sprite(resourceFileMap.home.horn));
-            this.addChild(this._pen = new cc.Sprite(resourceFileMap.home.pen));
+            var elementMap = this._animateElementMap = {};
+            this.addChild(elementMap['board'] = new cc.Sprite(resourceFileMap.home.board));
+            this.addChild(elementMap['title'] = new cc.Sprite(resourceFileMap.home.title));
+            this.addChild(elementMap['leftPerson'] = new cc.Sprite(resourceFileMap.home.leftPerson));
+            this.addChild(elementMap['rightPerson'] = new cc.Sprite(resourceFileMap.home.rightPerson));
+            this.addChild(elementMap['ribbon'] = new cc.Sprite(resourceFileMap.home.ribbon));
+            this.addChild(elementMap['button'] = buttonMenu);
+            this.addChild(elementMap['horn'] = new cc.Sprite(resourceFileMap.home.horn));
+            this.addChild(elementMap['pen'] = new cc.Sprite(resourceFileMap.home.pen));
 
             this._startAnimation();
         },
@@ -32,11 +33,12 @@ define([
             var winSize = cc.director.getWinSize();
             var center = cc.p(winSize.width / 2, winSize.height / 2);
             var outerDistance = 500;
+            var elementMap = this._animateElementMap;
             var startPositionMap = {
-                board: cc.p(center.x, winSize + outerDistance),
-                title: cc.p(center.x, winSize + outerDistance),
+                board: cc.p(center.x, winSize.height + outerDistance),
+                title: cc.p(center.x, winSize.height + outerDistance),
                 leftPerson: cc.p(-outerDistance, 640 - 212.5),
-                rightPerson: cc.p(outerDistance, 640 - 212.5),
+                rightPerson: cc.p(winSize.width + outerDistance, 640 - 212.5),
                 ribbon: cc.p(center.x, -outerDistance),
                 button: cc.p(center.x, -outerDistance)
             };
@@ -51,7 +53,38 @@ define([
             var hornPosition = cc.p(center.x - 165, 640 - 188);
             var penPosition = cc.p(center.x + 196.5, 640 - 172.5);
 
+            //起始位置
+            for (var key in startPositionMap) {
+                elementMap[key].setPosition(startPositionMap[key]);
+            }
+            elementMap.horn.setPosition(hornPosition);
+            elementMap.horn.setVisible(false);
+            elementMap.pen.setPosition(penPosition);
+            elementMap.pen.setVisible(false);
 
+            var time = 0.4;
+            //TODO: 加延时、回调（里加比如展示喇叭等）、ease参数
+            var keys = [
+                "board",
+                "title",
+                "leftPerson",
+                "rightPerson",
+                "ribbon",
+                "button"
+            ];
+            function moveInNextElement () {
+                //var ease = cc.easeElasticOut(.6);
+                var ease = cc.easeIn(2);
+                var key = keys.shift();
+                if (!key) { return }
+                var el = elementMap[key];
+                el.runAction(new cc.Sequence(
+                    (new cc.MoveTo(time, endPositionMap[key])).easing(ease),
+                    //(new cc.MoveTo(time, endPositionMap[key])),
+                    new cc.CallFunc(moveInNextElement)
+                ))
+            }
+            moveInNextElement();
         }
     });
 });
