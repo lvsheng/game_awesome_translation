95aa8b181fdd18415c5aa14aaa3c393b7e0a0ab6
lvsheng <lvsheng@baidu.com>
2015-01-05 17:31:48 +0800

    temp

--- src/home/HomeScene.js
+++ src/home/HomeScene.js
@@ -6,7 +6,7 @@
 define([
     '../gameUtil/resourceFileList',
     '../gameUtil/preload',
-    '../list/listScene',
+    '../listScene',
     './AnimateLayer',
     './BackgroundLayer'
 ], function (resourceFileList, preload, listScene, AnimateLayer, BackgroundLayer) {
---
+++ src/listScene.js
@@ -0,0 +1,23 @@
+/**
+ * 单例
+ * @author lvsheng
+ * @date 2015/1/3
+ */
+define([
+    'gameUtil/resourceFileMap',
+    './ListLayer',
+    './BackgroundLayer'
+], function (resourceFileMap, ListLayer, BackgroundLayer) {
+    var instance;
+    var ListScene =  cc.Scene.extend({
+        onEnter: function () {
+            var self = this;
+            self._super();
+
+            self.addChild(new BackgroundLayer());
+            self.addChild(new ListLayer());
+        }
+    });
+
+    return { getInstance: function(){ return instance ? instance : (instance = new ListScene()); } };
+});
