9922795fcbdf41e452bea219fb3c01d36d860489
lvsheng <lvsheng@baidu.com>
2015-01-05 15:36:18 +0800

    尝试用异步的方式解决同步时拿到的sprite的宽度为0的问题

--- src/home/BackgroundLayer.js
+++ src/home/BackgroundLayer.js
@@ -9,8 +9,12 @@ define([
             this._backgroundSprite = new cc.Sprite(resourceFileMap.home.bg);
             var winSize = cc.director.getWinSize();
             this._backgroundSprite.attr({ anchorX: 0.5, anchorY: 1, x: winSize.width / 2, y: winSize.height });
-            this._scaleToCoverWindow(this._backgroundSprite);
-            this.addChild(this._backgroundSprite);
+
+            this.scheduleOnce(function(){
+                //这几句话如果直接执行，此时拿到的sprite的width是0。。。不清楚为什么？异步渲染的估计。。。但神奇的是其他背景层中没有遇到这个问题。。。
+                this._scaleToCoverWindow(this._backgroundSprite);
+                this.addChild(this._backgroundSprite);
+            });
         },
 
         _scaleToCoverWindow: function (sprite) {
