864de3da889018adcec35fe3583b5ffcab3ac05a
lvsheng <lvsheng@baidu.com>
2015-01-06 23:50:53 +0800

    改为这次的结构

--- src/games/hit/GameScene.js
+++ src/games/hit/GameScene.js
@@ -4,20 +4,27 @@
  */
 define([
     './BackgroundLayer',
+    './GuideLayer',
     './gameLayer/GameLayer',
     './scoreManager',
-    '../../commonClass/ResultLayer'
-], function (BackgroundLayer, GameLayer, scoreManager, ResultLayer) {
+    '../../commonClass/ResultLayer',
+    '../../commonClass/MenuLayer'
+], function (BackgroundLayer, GuideLayer, GameLayer, scoreManager, ResultLayer, MenuLayer) {
     return cc.Scene.extend({
         onEnter: function () {
             var self = this;
             self._super();
 
             self.addChild(new BackgroundLayer());
-            self.addChild(new GameLayer(function(result){
-                result = {amount: scoreManager.getScore()};
-                self.addChild(new ResultLayer("你成功打掉了" + result.amount + "个地鼠"));
+            self.addChild(new GuideLayer(function(){
+                self.addChild(new GameLayer(function(result){
+                    result = {amount: scoreManager.getScore()};
+                    self.addChild(new ResultLayer("你成功打掉了" + result.amount + "个地鼠"));
+                }));
+                self.addChild(self._menuLayer = new MenuLayer());
             }));
-        }
+        },
+        pauseGame: function () { this._menuLayer && this._menuLayer.pauseGame(); },
+        resumeGame: function () { this._menuLayer && this._menuLayer.resumeGame(); }
     });
 });
---
+++ src/games/hit/GuideLayer.js
@@ -0,0 +1,38 @@
+/**
+ * 引导层、提示用户本游戏的玩法
+ */
+define([
+    '../../util/resourceFileMap',
+    '../../commonClass/GuideDialog'
+], function (resourceFileMap, GuideDialog) {
+    return cc.Layer.extend({
+        _onStartGame: null,
+
+        /**
+         * @constructor
+         * @param onStartGame 回调函数，用户确认跳过引导层时执行
+         */
+        ctor: function (onStartGame) {
+            var self = this;
+            self._super();
+            self.init();
+
+            self._onStartGame = onStartGame;
+
+            self.addChild(new GuideDialog(
+                "lover",
+                "收集代表爱情的心，拉近单身狗与女神的距离",
+                function(){ self._startGame(); }
+            ));
+        },
+
+        /**
+         * 结束引导，开始游戏
+         * @private
+         */
+        _startGame:function () {
+            this.parent.removeChild(this);
+            this._onStartGame();
+        }
+    });
+});
