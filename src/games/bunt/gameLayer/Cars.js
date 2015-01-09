68c5e5e6bd9e7092d3f47be4087eb5188b712f5d
lvsheng <lvsheng@baidu.com>
2015-01-09 23:47:07 +0800

    调整结束动画

--- src/games/bunt/gameLayer/Cars.js
+++ src/games/bunt/gameLayer/Cars.js
@@ -6,39 +6,40 @@ define([
     '../../../util/resourceFileMap'
 ], function (PositionManager, ComputerControl, UserControl, FasterLayer, resourceFileMap) {
     return cc.Sprite.extend({
-        _onWinningOrLoosing: function(){},
-        _positionManager: null,
-        _carsSprite: null,
-        _computerControl: null,
-        _userControl: null,
-        _curLevel: 0,
-        _winAmount: 0,
-
         ctor: function (onWinningOrLoosing) {
             var self = this;
             self._super();
             self.init();
 
+            self._curLevel = 0;
+            self._winAmount = 0;
             self._onWinningOrLoosing = onWinningOrLoosing;
+            self._positionManager = null;
+            self._carsSprite = null;
+            self._computerControl = null;
+            self._userControl = null;
+
             self.addChild(self._positionManager = new PositionManager(function(winner){
                 if (winner === 'right') { ++ self._winAmount; }
 
-                if (self._curLevel === 2 && winner === 'right') { //3局都赢了
-                    self._onWinningOrLoosing(true);
-                } else if (winner === 'right') { //赢了当前局，进入下一局
-                    self.removeChild(self._computerControl);
-                    self._userControl.pauseTouch();
-                    self._positionManager.restore();
+                self._userControl.pauseTouch();
+                self.removeChild(self._computerControl);
 
-                    self.addChild(self._fasterLayer = new FasterLayer(function(){
-                        self.removeChild(self._fasterLayer);
-                        self._userControl.restoreTouch();
-                        ++self._curLevel;
-                        self.addChild(self._computerControl = new ComputerControl(self._positionManager, self._curLevel));
-                    }, self._curLevel));
-                } else {
-                    self._onWinningOrLoosing(false);
-                }
+                self._blink(function () {
+                    if (self._curLevel === 2 && winner === 'right') { //3局都赢了
+                        self._onWinningOrLoosing(true);
+                    } else if (winner === 'right') { //赢了当前局，进入下一局
+                        self.addChild(self._fasterLayer = new FasterLayer(function(){
+                            self.removeChild(self._fasterLayer);
+                            self._fasterLayer = null;
+                            ++self._curLevel;
+                            self.addChild(self._computerControl = new ComputerControl(self._positionManager, self._curLevel));
+                            self._userControl.restoreTouch();
+                        }, self._curLevel));
+                    } else { //输了
+                        self._onWinningOrLoosing(false);
+                    }
+                });
             }));
             self.addChild(self._carsSprite = new cc.Sprite(resourceFileMap.bunt.cars));
             self.addChild(self._userControl = new UserControl(self._positionManager));
@@ -51,6 +52,21 @@ define([
         getUserHitCount: function () { return this._userControl.getHitCount(); },
         getWinAmount: function () {
             return this._winAmount;
+        },
+
+        _blink: function (callback) {
+            var self = this;
+            self._carsSprite.runAction(new cc.Sequence(
+                new cc.DelayTime(0.3),
+                new cc.Blink(0.3, 3),
+                new cc.FadeOut(0.8),
+                new cc.CallFunc(function(){
+                    self._carsSprite.opacity = 255;
+                    self._positionManager.restore();
+                    self._updatePosition();
+                    callback();
+                })
+            ));
         },
 
         _updatePosition: function () {
