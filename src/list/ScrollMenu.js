197d17b4d7d54ff89eafaf5e3c9f80efc9bee165
lvsheng <lvsheng@baidu.com>
2015-01-09 09:27:06 +0800

    从cocos源码里拷贝了menu的各事件处理过来

--- src/list/ScrollMenu.js
+++ src/list/ScrollMenu.js
@@ -8,6 +8,67 @@ define([], function () {
             this._super();
             cc.associateWithNative(this, cc.Layer);
             this._touchListener.setSwallowTouches(false);
+        },
+        _onTouchBegan: function (touch, event) {
+            var target = event.getCurrentTarget();
+            if (target._state != cc.MENU_STATE_WAITING || !target._visible || !target.enabled)
+                return false;
+            for (var c = target.parent; c != null; c = c.parent) {
+                if (!c.isVisible())
+                    return false;
+            }
+            target._selectedItem = target._itemForTouch(touch);
+            if (target._selectedItem) {
+                target._state = cc.MENU_STATE_TRACKING_TOUCH;
+                target._selectedItem.selected();
+                target._selectedItem.setNodeDirty();
+                return true;
+            }
+            return false;
+        },
+        _onTouchEnded: function (touch, event) {
+            var target = event.getCurrentTarget();
+            if (target._state !== cc.MENU_STATE_TRACKING_TOUCH) {
+                cc.log("cc.Menu.onTouchEnded(): invalid state");
+                return;
+            }
+            if (target._selectedItem) {
+                target._selectedItem.unselected();
+                target._selectedItem.setNodeDirty();
+                target._selectedItem.activate();
+            }
+            target._state = cc.MENU_STATE_WAITING;
+        },
+        _onTouchCancelled: function (touch, event) {
+            var target = event.getCurrentTarget();
+            if (target._state !== cc.MENU_STATE_TRACKING_TOUCH) {
+                cc.log("cc.Menu.onTouchCancelled(): invalid state");
+                return;
+            }
+            if (this._selectedItem) {
+                target._selectedItem.unselected();
+                target._selectedItem.setNodeDirty();
+            }
+            target._state = cc.MENU_STATE_WAITING;
+        },
+        _onTouchMoved: function (touch, event) {
+            var target = event.getCurrentTarget();
+            if (target._state !== cc.MENU_STATE_TRACKING_TOUCH) {
+                cc.log("cc.Menu.onTouchMoved(): invalid state");
+                return;
+            }
+            var currentItem = target._itemForTouch(touch);
+            if (currentItem != target._selectedItem) {
+                if (target._selectedItem) {
+                    target._selectedItem.unselected();
+                    target._selectedItem.setNodeDirty();
+                }
+                target._selectedItem = currentItem;
+                if (target._selectedItem) {
+                    target._selectedItem.selected();
+                    target._selectedItem.setNodeDirty();
+                }
+            }
         }
     });
     ScrollMenu.create = function () {
