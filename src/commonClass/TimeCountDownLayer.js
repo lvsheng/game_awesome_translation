a2309b442a3430fa1b3fc6b76587ba3cb19cd603
lvsheng <lvsheng@baidu.com>
2015-01-12 18:15:57 +0800

    清缓存，恢复图片

--- src/commonClass/TimeCountDownLayer.js
+++ src/commonClass/TimeCountDownLayer.js
@@ -19,8 +19,9 @@ define([
             var board = new cc.Sprite(resourceFileMap.common.countBoard);
             board.attr({anchorY: 1, y: winSize.height, x: 215});
             self.addChild(board);
+            //alert("board path:" + resourceFileMap.common.countBoard);
 
-            var label = new cc.LabelBMFont("时间有吗？", resourceFileMap.common.resultLayer.textFont);
+            var label = new cc.LabelBMFont("", resourceFileMap.common.resultLayer.textFont);
             label.color = cc.color(255, 255, 255, 255);
             label.attr({anchorX: 0, anchorY: 0.5});
             label.setPosition(207, winSize.height - 61);
